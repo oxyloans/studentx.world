b1954ef1c42a6467b0f45687c6dc47a2485ad358
Liveen mandeva <livinmandeva@gmail.com>
2023-03-27 17:31:52 +0530

    student admin

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1782802a99c18e29ad239902341e1910.js"
+  "/precache-manifest.8e233cfba56d8573c173174288d1e37a.js"
 );
 
 self.addEventListener('message', (event) => {
